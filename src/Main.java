bbcc1c4ebf25eac1f6b5f5ec65c74ccf23998f41
Joseph Park <vapor47@gmail.com>
2018-09-02 00:10:18 -0500

    Added Setup Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args){
@@ -8,7 +7,6 @@ public class Main {
         System.out.println("Welcome to Risk!\nHow many people are playing?(2-6)");
         int numPlayers = input.nextInt();
 
-//        setup(numPlayers);
 
         while(true){ //until only 1 player occupies territories(except for neutral in 2 player games)
             /*
