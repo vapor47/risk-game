198375d197b974b243ad072f8eea0d7929363dc6
Joseph Park <vapor47@gmail.com>
2018-09-01 23:55:01 -0500

    Added Main class

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Risk!\nHow many people are playing?(2-6)");
+        int numPlayers = input.nextInt();
+
+//        setup(numPlayers);
+
+        while(true){ //until only 1 player occupies territories(except for neutral in 2 player games)
+            /*
+            cycle through player turns
+             */
+        }
+    }
+}
