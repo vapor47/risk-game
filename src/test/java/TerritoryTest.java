088ccff9e3046a66d7a219d764f4db4e9ced9aa8
Joseph Park <vapor47@gmail.com>
2018-09-20 13:18:57 -0500

    added couple unit tests for territory

---
+++ src/test/java/TerritoryTest.java
@@ -0,0 +1,78 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TerritoryTest {
+
+    @Test
+    public void incrementArmies() {
+        // Arrange
+        final int expected = 4;
+        Territory test = new Territory("Test Name","Test Continent", new String[]{"Adjacent Territory"});
+
+        // Act
+        test.incrementArmies(4);
+        final int actual = test.getNumArmies();
+
+        // Assert
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void decrementArmies() {
+        // Arrange
+        final int expected = 5;
+        Territory test = new Territory("Test Name","Test Continent", 7,
+                new Player("Test Player"), new String[]{"Adjacent Territory"});
+
+        // Act
+        test.decrementArmies(2);
+        final int actual = test.getNumArmies();
+
+        // Assert
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void setOwner() {
+    }
+
+    @Test
+    public void getTerritoryName() {
+    }
+
+    @Test
+    public void getContinent() {
+    }
+
+    @Test
+    public void getNumArmies() {
+        // Arrange
+        final int expected = 3;
+        Territory test = new Territory("Test Name","Test Continent", 3,
+                new Player("Test Player"), new String[]{"Adjacent Territory"});
+
+        // Act
+        final int actual = test.getNumArmies();
+
+        // Assert
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void getOwner() {
+    }
+
+    @Test
+    public void getAdjacentTerritories() {
+    }
+
+    @Test
+    public void printAdjacentTerritories() {
+    }
+
+    @Test
+    public void listTerritoryInfo() {
+    }
+}
