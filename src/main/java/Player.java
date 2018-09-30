38297f171bd77c0f26f678a5eadfd3506de0771f
Phillip Nguyen <phillipnguyen0421@gmail.com>
2018-09-30 17:26:55 -0500

    Updated Player & TerritoryList bugs

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Player 
 {
-    private static int cardValue = 0; //keeps track of all cards traded in
-    private static Deck deck; //The deck of cards shared among players
-    //Note: I dont know what kind of value we are using for territories here, int is just a place holder
+    private static int cardValue = 0; //keeps track of all cards traded in    
+
     TerritoryList territories = new TerritoryList();
     
     private boolean claimCheck = false; 
     
     private String playerName;
     
-    public Card hand[] = new Card[6]; //The players currnet cards
+    public Card hand[] = new Card[11]; //The players currnet cards
    
     private byte cardCount; //The current number of cards the player holds
     private int placeableInfantry;
@@ -27,14 +26,17 @@ public class Player
     {
         territories.printConts();
     }
+
     public String getPlayerName() 
     {
         return playerName;
     }
+
     public int getPlaceableInfantry()
     {
         return placeableInfantry;
     }
+
     public void updatePlaceableInfantry(int infantry) 
     {
         placeableInfantry += infantry;
@@ -79,7 +81,10 @@ public class Player
                  
         else if(cardA.getType() != cardC.getType() && cardB.getType() != cardC.getType())
             return true;
-        
+
+        else if(cardA.getType() == Type.WILD || cardB.getType() == Type.WILD || cardC.getType() == Type.WILD)
+            return true;
+
         return false;
     }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,7 +96,7 @@ public class Player
         int specialCards[] = new int[3];
         for(int i = 0; i < 3; i++)
         {
-            if(territories.hasTerriotory(hand[cardIndex[i]].getTerritory()))
+            if(territories.hasTerritory(hand[cardIndex[i]].getTerritory()))
             {
                 specialCards[numSpecial] = cardIndex[i];
                 numSpecial++;
@@ -100,7 +105,7 @@ public class Player
         if(numSpecial > 0)
         {
             int tIndex;
-            System.out.printf("Chose which terrirtory you would like to place two extra armies on(1=%d)%n", numSpecial);
+            System.out.printf("Chose which territory you would like to place two extra armies on(1=%d)%n", numSpecial);
             printSpecialCards(specialCards, numSpecial);
             for(tIndex = input.nextInt(); tIndex <= 0 || tIndex > numSpecial; tIndex = input.nextInt())
             {
@@ -124,7 +129,7 @@ public class Player
         {
             if(hand[i] != null)
             {
-                replacement[j] = hand[i];
+                replacement[j++] = hand[i];
             } 
         }
         hand = replacement;
@@ -134,7 +139,7 @@ public class Player
     private void printSpecialCards(int specialCards[], int numSpecial)
     {
         for(int i = 0; i < numSpecial; i++)
-            System.out.printf("Card %d: %s%n", i, hand[i].getTerritory());
+            System.out.printf("Card %d: %s%n", i + 1, hand[i].getTerritory());
     }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public int playHand()
@@ -173,6 +178,7 @@ public class Player
     public void placeInfantry(String t, int inf)
     {
         Main.territories.get(t).incrementArmies(inf);
+        placeableInfantry -= inf;
     }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void claimTerritory(Territory t)
@@ -228,7 +234,7 @@ public class Player
     {
         if(claimCheck)
         {
-            hand[cardCount++] = deck.draw();
+            hand[cardCount++] = Main.deck.draw();
             System.out.printf("Player %s has drawn a card, Territory: %s, Army: %s%n", playerName,hand[cardCount-1].getTerritory(), hand[cardCount-1].getType());
             claimCheck = false;
         }
@@ -289,15 +295,10 @@ public class Player
                 Main.territories.get(Attacker.getTerritoryName()).decrementArmies(1);
         }
         if(Defender.getNumArmies() == 0)
-        {            
-            System.out.printf("Congratulations %s, you have conquered %s!%n", Attacker.getOwner().getName(), Defender.getOwner().getName());
+        {
             claimTerritory(Defender);
-<<<<<<< HEAD
-            DefendingPlayer.loseTerritory(Defender.getTerritoryName());
-=======
-            Main.playerMap.get(Defender.getOwner()).loseTerritory(Defender);
+            Main.playerMap.get(Defender.getOwner().getName()).loseTerritory(Defender);
             System.out.printf("Congratulations player %s, you have conquered %s!%n", Attacker.getOwner(), Defender.getOwner());
->>>>>>> master
             moveInArmies(Attacker, Defender);
             claimCheck = true;
         }
