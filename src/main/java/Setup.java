bcaef7c6b2cd9bc0124c97494c07a96b0dbea3e0
Joseph Park <vapor47@gmail.com>
2018-09-30 19:56:32 -0500

    deleted duplicate territory map from Setup

--- src/main/java/Setup.java
+++ src/main/java/Setup.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Setup {
     private int numPlayers;
-    private final Map<String,Territory> territories = new HashMap();
     private Deck deck = new Deck(); // Creates a deck object
     private int startingPlayer = chooseRandomPlayer(numPlayers);
 
@@ -33,11 +32,10 @@ public class Setup {
         }
 
         // test
-        for(Map.Entry<String, Territory> x: territories.entrySet()){
+        for(Map.Entry<String, Territory> x: Main.territories.entrySet()){
             System.out.print(x.getValue().getTerritoryName() + " || ");
             System.out.println(x.getValue().getOwner().getPlayerName());
         }
-
     }
 
     public int getStartingPlayerIndex() {
@@ -48,35 +46,35 @@ public class Setup {
         return numPlayers;
     }
     /* Two Player Set-up
-            give both players 14 random territories and place 1 infantry on them
+            give both players 14 random Main.territories and place 1 infantry on them
                 same for neutral
             then:
                 go back and forth between both players
-                    1. place 2 infantry on any 1 or 2 territories
+                    1. place 2 infantry on any 1 or 2 Main.territories
                         either 1 & 1, or 2 infantry on 1 territory
                     2. place 1 infantry on any neutral territory
     */
     private void twoPlayerStart(Player neutral){
         /*
-            Get array of territories(Map) keys
-            Get a random key and assign it to a player and move to next player - repeat 28 times(14 territories for each player)
-            Other territories are neutral by default
+            Get array of Main.territories(Map) keys
+            Get a random key and assign it to a player and move to next player - repeat 28 times(14 Main.territories for each player)
+            Other Main.territories are neutral by default
          */
-        ArrayList<String> keys = new ArrayList<String>(territories.keySet());
+        ArrayList<String> keys = new ArrayList<String>(Main.territories.keySet());
         int currPlayerIndex = 0;
         for(int i = 42; i > 14; i--){ // runs 28 times
             int index = (int)(Math.random()*i);
             String key = keys.get(index);
-            territories.get(key).incrementArmies(1);
-            territories.get(key).setOwner(Main.playerMap.get(Main.playerList.get(currPlayerIndex)));
+            Main.territories.get(key).incrementArmies(1);
+            Main.territories.get(key).setOwner(Main.playerMap.get(Main.playerList.get(currPlayerIndex)));
             Main.playerMap.get(Main.playerList.get(currPlayerIndex)).updatePlaceableInfantry(-1);
 
             currPlayerIndex = (currPlayerIndex + 1) % 2; // goes from 0-1
             keys.remove(index);
         }
-        // set remaining Neutral territories armies to 1
+        // set remaining Neutral Main.territories armies to 1
         for(String territoryName : keys){
-            territories.get(territoryName).incrementArmies(1);
+            Main.territories.get(territoryName).incrementArmies(1);
         }
     }
 
@@ -92,19 +90,19 @@ public class Setup {
             do {
                 System.out.print("\n" + currPlayerName + ", claim a territory: ");
                 chosenTerritory = input.nextLine();
-                if(!territories.containsKey(chosenTerritory) || !territories.get(chosenTerritory).getOwner().getPlayerName().equals("Neutral")) {
+                if(!Main.territories.containsKey(chosenTerritory) || !Main.territories.get(chosenTerritory).getOwner().getPlayerName().equals("Neutral")) {
                     if(chosenTerritory.equals("list-unclaimed"))
                         listUnclaimedTerritories();
                     else {
                         System.out.println("That is an invalid option.\n" +
-                                "For a list of unclaimed territories, type 'list-unclaimed'");
+                                "For a list of unclaimed Main.territories, type 'list-unclaimed'");
                     }
                 } else { // claim territory for current player
-                    Main.playerMap.get(currPlayerName).claimTerritory(territories.get(chosenTerritory));
-                    territories.get(chosenTerritory).incrementArmies(1);
+                    Main.playerMap.get(currPlayerName).claimTerritory(Main.territories.get(chosenTerritory));
+                    Main.territories.get(chosenTerritory).incrementArmies(1);
                     Main.playerMap.get(currPlayerName).updatePlaceableInfantry(-1);
                 }
-            } while(!territories.containsKey(chosenTerritory) || !territories.get(chosenTerritory).getOwner().getPlayerName().equals("Neutral"));
+            } while(!Main.territories.containsKey(chosenTerritory) || !Main.territories.get(chosenTerritory).getOwner().getPlayerName().equals("Neutral"));
             // move to next player
             currPlayerIndex = setPrevPlayer(currPlayerIndex);
         }
@@ -117,17 +115,17 @@ public class Setup {
                 chosenTerritory = input.nextLine();
 
                 // checks to see if player is owner of the territory
-                if(Main.playerMap.get(currPlayerName) != territories.get(chosenTerritory).getOwner()) {
+                if(Main.playerMap.get(currPlayerName) != Main.territories.get(chosenTerritory).getOwner()) {
                     if(chosenTerritory.equals("list-owned"))
                         Main.playerMap.get(currPlayerName).printOwnedTerritories();
                     else {
                         System.out.println("That is an invalid option.\n" +
-                                "For a list of owned territories, type 'list-owned'");
+                                "For a list of owned Main.territories, type 'list-owned'");
                     }
                 } else { // claim territory for current player
-                    territories.get(chosenTerritory).incrementArmies(1);
+                    Main.territories.get(chosenTerritory).incrementArmies(1);
                 }
-            } while(!territories.containsKey(chosenTerritory));
+            } while(!Main.territories.containsKey(chosenTerritory));
             currPlayerIndex = setPrevPlayer(currPlayerIndex);
         }
     }
@@ -148,7 +146,7 @@ public class Setup {
     }
 
     private void listUnclaimedTerritories(){
-        for(Map.Entry<String, Territory> x : territories.entrySet()){
+        for(Map.Entry<String, Territory> x : Main.territories.entrySet()){
             if(x.getValue().getNumArmies() == 0)
                 System.out.println(x.getKey());
         }
@@ -194,105 +192,105 @@ public class Setup {
     }
     private void createTerritories(){
         // North America
-        territories.put("Alaska", new Territory("Alaska", "North America",
+        Main.territories.put("Alaska", new Territory("Alaska", "North America",
                 new String[]{"Alberta","Kamchatka","North West Territory"}));
-        territories.put("Alberta", new Territory("Alberta", "North America",
+        Main.territories.put("Alberta", new Territory("Alberta", "North America",
                 new String[]{"Alaska","North West Territory","Ontario","Western United States"}));
-        territories.put("Central America", new Territory("Central America", "North America",
+        Main.territories.put("Central America", new Territory("Central America", "North America",
                 new String[]{"Eastern United States","Venezuela","Western United States"}));
-        territories.put("Eastern United States", new Territory("Eastern United States", "North America",
+        Main.territories.put("Eastern United States", new Territory("Eastern United States", "North America",
                 new String[]{"Central America","Ontario","Quebec","Western United States"}));
-        territories.put("Greenland", new Territory("Greenland", "North America",
+        Main.territories.put("Greenland", new Territory("Greenland", "North America",
                 new String[]{"Iceland","North West Territory","Ontario","Quebec"}));
-        territories.put("North West Territory", new Territory("North West Territory", "North America",
+        Main.territories.put("North West Territory", new Territory("North West Territory", "North America",
                 new String[]{"Alaska","Alberta","Greenland","Ontario"}));
-        territories.put("Ontario", new Territory("Ontario", "North America",
+        Main.territories.put("Ontario", new Territory("Ontario", "North America",
                 new String[]{"Alberta","Eastern United States","Greenland","North West Territory","Quebec","Western United States"}));
-        territories.put("Quebec", new Territory("Quebec", "North America",
+        Main.territories.put("Quebec", new Territory("Quebec", "North America",
                 new String[]{"Eastern United States","Greenland","Ontario"}));
-        territories.put("Western United States", new Territory("Western United States", "North America",
+        Main.territories.put("Western United States", new Territory("Western United States", "North America",
                 new String[]{"Alberta","Central America","Eastern United States","Ontario"}));
 
         // South America
-        territories.put("Argentina", new Territory("Argentina", "South America",
+        Main.territories.put("Argentina", new Territory("Argentina", "South America",
                 new String[]{"Brazil","Peru"}));
-        territories.put("Brazil", new Territory("Brazil", "South America",
+        Main.territories.put("Brazil", new Territory("Brazil", "South America",
                 new String[]{"Argentina","North Africa","Peru","Venezuela"}));
-        territories.put("Peru", new Territory("Peru", "South America",
+        Main.territories.put("Peru", new Territory("Peru", "South America",
                 new String[]{"Argentina","Brazil","Venezuela"}));
-        territories.put("Venezuela", new Territory("Venezuela", "South America",
+        Main.territories.put("Venezuela", new Territory("Venezuela", "South America",
                 new String[]{"Brazil","Central America","Peru"}));
 
         // Europe
-        territories.put("Great Britain", new Territory("Great Britain", "Europe",
+        Main.territories.put("Great Britain", new Territory("Great Britain", "Europe",
                 new String[]{"Iceland","Northern Europe","Scandinavia","Western Europe"}));
-        territories.put("Iceland", new Territory("Iceland", "Europe",
+        Main.territories.put("Iceland", new Territory("Iceland", "Europe",
                 new String[]{"Great Britain","Greenland","Scandinavia"}));
-        territories.put("Northern Europe", new Territory("Northern Europe", "Europe",
+        Main.territories.put("Northern Europe", new Territory("Northern Europe", "Europe",
                 new String[]{"Great Britain","Scandinavia","Southern Europe","Ukraine","Western Europe"}));
-        territories.put("Scandinavia", new Territory("Scandinavia", "Europe",
+        Main.territories.put("Scandinavia", new Territory("Scandinavia", "Europe",
                 new String[]{"Great Britain","Iceland","Northern Europe","Ukraine"}));
-        territories.put("Southern Europe", new Territory("Southern Europe", "Europe",
+        Main.territories.put("Southern Europe", new Territory("Southern Europe", "Europe",
                 new String[]{"Egypt","Middle East","North Africa","Northern Europe","Ukraine","Western Europe"}));
-        territories.put("Ukraine", new Territory("Ukraine", "Europe",
+        Main.territories.put("Ukraine", new Territory("Ukraine", "Europe",
                 new String[]{"Afghanistan","Middle East","Northern Europe","Scandinavia","Southern Europe","Ural"}));
-        territories.put("Western Europe", new Territory("Western Europe", "Europe",
+        Main.territories.put("Western Europe", new Territory("Western Europe", "Europe",
                 new String[]{"Great Britain","North Africa","Northern Europe","Southern Europe"}));
 
         // Africa
-        territories.put("Congo", new Territory("Congo", "Africa",
+        Main.territories.put("Congo", new Territory("Congo", "Africa",
                 new String[]{"East Africa","North Africa","South Africa"}));
-        territories.put("East Africa", new Territory("East Africa", "Africa",
+        Main.territories.put("East Africa", new Territory("East Africa", "Africa",
                 new String[]{"Congo","Egypt","Madagascar","Middle East","North Africa","South Africa"}));
-        territories.put("Egypt", new Territory("Egypt", "Africa",
+        Main.territories.put("Egypt", new Territory("Egypt", "Africa",
                 new String[]{"East Africa","Middle East","North Africa","Southern Europe"}));
-        territories.put("Madagascar", new Territory("Madagascar", "Africa",
+        Main.territories.put("Madagascar", new Territory("Madagascar", "Africa",
                 new String[]{"East Africa","South Africa"}));
-        territories.put("North Africa", new Territory("North Africa", "Africa",
+        Main.territories.put("North Africa", new Territory("North Africa", "Africa",
                 new String[]{"Brazil","Congo","East Africa","Egypt","Southern Europe","Western Europe"}));
-        territories.put("South Africa", new Territory("South Africa", "Africa",
+        Main.territories.put("South Africa", new Territory("South Africa", "Africa",
                 new String[]{"Congo","East Africa","Madagascar"}));
 
         // Asia
-        territories.put("Afghanistan", new Territory("Afghanistan", "Asia",
+        Main.territories.put("Afghanistan", new Territory("Afghanistan", "Asia",
                 new String[]{"China","India","Middle East","Ukraine","Ural"}));
-        territories.put("China", new Territory("China", "Asia",
+        Main.territories.put("China", new Territory("China", "Asia",
                 new String[]{"Afghanistan","India","Mongolia","Siam","Siberia","Ural"}));
-        territories.put("India", new Territory("India", "Asia",
+        Main.territories.put("India", new Territory("India", "Asia",
                 new String[]{"Afghanistan","China","Middle East","Siam"}));
-        territories.put("Irkutsk", new Territory("Irkutsk", "Asia",
+        Main.territories.put("Irkutsk", new Territory("Irkutsk", "Asia",
                 new String[]{"Kamchatka","Mongolia","Siberia","Yakutsk"}));
-        territories.put("Japan", new Territory("Japan", "Asia",
+        Main.territories.put("Japan", new Territory("Japan", "Asia",
                 new String[]{"Kamchatka","Mongolia"}));
-        territories.put("Kamchatka", new Territory("Kamchatka", "Asia",
+        Main.territories.put("Kamchatka", new Territory("Kamchatka", "Asia",
                 new String[]{"Alaska","Irkutsk","Japan","Mongolia","Yakutsk"}));
-        territories.put("Middle East", new Territory("Middle East", "Asia",
+        Main.territories.put("Middle East", new Territory("Middle East", "Asia",
                 new String[]{"Afghanistan","East Africa","Egypt","India","Southern Europe","Ukraine"}));
-        territories.put("Mongolia", new Territory("Mongolia", "Asia",
+        Main.territories.put("Mongolia", new Territory("Mongolia", "Asia",
                 new String[]{"China","Irkutsk","Japan","Kamchatka","Siberia"}));
-        territories.put("Siam", new Territory("Siam", "Asia",
+        Main.territories.put("Siam", new Territory("Siam", "Asia",
                 new String[]{"China","India","Indonesia"}));
-        territories.put("Siberia", new Territory("Siberia", "Asia",
+        Main.territories.put("Siberia", new Territory("Siberia", "Asia",
                 new String[]{"China","Irkutsk","Mongolia","Ural","Yakutsk"}));
-        territories.put("Ural", new Territory("Ural", "Asia",
+        Main.territories.put("Ural", new Territory("Ural", "Asia",
                 new String[]{"Afghanistan","China","Siberia","Ukraine"}));
-        territories.put("Yakutsk", new Territory("Yakutsk", "Asia",
+        Main.territories.put("Yakutsk", new Territory("Yakutsk", "Asia",
                 new String[]{"Irkutsk","Kamchatka","Siberia"}));
 
         // Australia
-        territories.put("Eastern Australia", new Territory("Eastern Australia", "Australia",
+        Main.territories.put("Eastern Australia", new Territory("Eastern Australia", "Australia",
                 new String[]{"New Guinea","Western Australia"}));
-        territories.put("Indonesia", new Territory("Indonesia", "Australia",
+        Main.territories.put("Indonesia", new Territory("Indonesia", "Australia",
                 new String[]{"New Guinea","Siam","Western Australia"}));
-        territories.put("New Guinea", new Territory("New Guinea", "Australia",
+        Main.territories.put("New Guinea", new Territory("New Guinea", "Australia",
                 new String[]{"Eastern Australia","Indonesia","Western Australia"}));
-        territories.put("Western Australia", new Territory("Western Australia", "Australia",
+        Main.territories.put("Western Australia", new Territory("Western Australia", "Australia",
                 new String[]{"Eastern Australia","Indonesia","New Guinea"}));
     }
 
     // Creates all the cards & place into deck object
     // Types = INFANTRY, CAVALRY, ARTILLERY, WILD
-    // 42 cards with territories + 2 wild cards = 44 cards
+    // 42 cards with Main.territories + 2 wild cards = 44 cards
     private void createCards() {        
         // North America: 9
         deck.addCards(new Card(Type.INFANTRY, "Alaska"));
